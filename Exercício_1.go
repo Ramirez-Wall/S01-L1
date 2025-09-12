2f10dfb040771a9edd2b246205a02a5c12cf31e5
Ramirez-Wall <uchihamake@hotmail.com>
2025-09-11 23:14:22 -0300

    Create Exercício_1.go

---
+++ Exercício_1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Função que retorna se o número é positivo, negativo ou zero
+func VerifNum(n int) string {
+	if n > 0 {
+		return "Positivo"
+	} else if n < 0 {
+		return "Negativo"
+	}
+	return "Zero"
+}
+
+func main() {
+	var numero int
+
+	fmt.Print("Digite um número: ")
+	fmt.Scan(&numero)
+
+	fmt.Println("O número é:", VerifNum(numero))
+}
